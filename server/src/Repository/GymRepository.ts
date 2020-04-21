2a5c7c12c0439b9511dcf33c5b9fe8c45c0c7d00
Noor Sheikh <noorsheikh.me@gmail.com>
2020-04-20 23:39:29 -0400

    query indentation fix

--- server/src/Repository/GymRepository.ts
+++ server/src/Repository/GymRepository.ts
@@ -8,18 +8,18 @@ export class GymRepository implements IGymRepository {
   async getAllGyms(id?: number): Promise<Gym[]> {
     return await (await getDBManager())
       .query(`
-      SELECT
-        g.name,
-        g.address,
-        g.city,
-        g.state,
-        g.zip_code as zipCode,
-        g.latitude,
-        g.longitude,
-        if(u.id = ${id}, true, false) as isSavedGym
-      FROM gym g
-      LEFT JOIN saved_gym sg ON sg.gym_id = g.id
-      LEFT JOIN user u ON sg.user_id = u.id
+        SELECT
+          g.name,
+          g.address,
+          g.city,
+          g.state,
+          g.zip_code as zipCode,
+          g.latitude,
+          g.longitude,
+          if(u.id = ${id}, true, false) as isSavedGym
+        FROM gym g
+        LEFT JOIN saved_gym sg ON sg.gym_id = g.id
+        LEFT JOIN user u ON sg.user_id = u.id
       `)
     ;
   }
